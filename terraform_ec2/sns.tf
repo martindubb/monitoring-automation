c7874c47bab4f7c0c27814ea8870ee350fe43c15
martin <martin@techstarter.de>
2023-11-14 09:51:17 +0100

    musterlösung montag - fixed

--- terraform_ec2/sns.tf
+++ terraform_ec2/sns.tf
@@ -2,7 +2,7 @@ resource "aws_sns_topic" "prom_alerts" {
   name = "prometheus-alerts"
 }
 
-resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
+resource "aws_sns_topic_subscription" "prom_alerts_email_sub" {
   topic_arn = aws_sns_topic.prom_alerts.arn
   protocol  = "email"
   endpoint  = "martin@techstarter.de"
